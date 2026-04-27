dfbd6a9033045414a944c12763379ec448379682
Radman <radikahn@gmail.com>
2026-04-26 23:23:00 -0700

    feat: modular refactorization
    Co-authored-by: IvantheGreatMaker <iamivanselvan@gmail.com>

--- main.py
+++ main.py
@@ -1,64 +1,16 @@
 import os
 
+from loader.banana import init_data
+from model.cnn import apply_cnn, fit_cnn
+
+
 def setup():
-    print("Setting up banana...")
-    import tensorflow as tf
-    from tensorflow.keras import layers, models
-    img_size = (224, 224)
-    batch_size = 32
-    dataset_base = "Banana Ripeness Classification Dataset"
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-        os.path.join(dataset_base, "train"),
-        image_size=img_size,
-        batch_size=batch_size,
-    )
+    train_ds, val_ds, test_ds = init_data(img_size=(224, 224), batch_size=32)
 
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-        os.path.join(dataset_base, "valid"),
-        image_size=img_size,
-        batch_size=batch_size,
-    )
+    model = apply_cnn(train_ds)
 
-    test_ds = tf.keras.utils.image_dataset_from_directory(
-        os.path.join(dataset_base, "test"),
-        image_size=img_size,
-        batch_size=batch_size,
-        shuffle=False,
-    )
-    print(train_ds.class_names)
+    fit_cnn(model, train_ds, val_ds)
 
-    #Simple CNN model 
-    model = models.Sequential([
-        layers.Input(shape=(224, 224, 3)),
-        layers.Rescaling(1./255),
-
-        layers.Conv2D(32, 3, activation='relu'),
-        layers.MaxPooling2D(),
-
-        layers.Flatten(),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(len(train_ds.class_names), activation='softmax')
-    ])
-
-    # Compile
-    model.compile(
-        optimizer='adam',
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
-    )
-
-    model.summary()
-
-    # Train (just a few epochs for testing)
-    model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=3
-    )
-
-
-
-        
 
 def main():
     print("Hello from banana!")
@@ -68,5 +20,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
