755d0a9e2e3fdc62c92b7024304b4d8707d3f3af
ehhdyyy <edmond13pan@gmail.com>
2026-05-03 13:40:43 -0700

    feat: added resnet50 pre-trained model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from loader.banana import init_data
 from model.cnn import apply_cnn, fit_cnn
 from model.rnn import apply_rnn, fit_rnn
+from model.resnet50 import apply_resnet, fit_resnet
 
 
 def plot_history(history):
@@ -33,19 +34,27 @@ def plot_history(history):
 def setup():
     train_ds, val_ds, test_ds = init_data(img_size=(224, 224), batch_size=32)
 
+    #CNN
+    print("Custom CNN Testing:")
     cnn_model = apply_cnn(train_ds)
     cnn_history = fit_cnn(cnn_model, train_ds, val_ds)
     plot_history(cnn_history)
 
+    #ResNet50
+    print("ResNet50 Testing")
+    resnet_model = apply_resnet(train_ds)
+    resnet_history = fit_resnet(resnet_model, train_ds, val_ds)
+    plot_history(resnet_history)
+
     # rnn_model = apply_rnn(train_ds)
     # fit_rnn(rnn_model, train_ds, val_ds)
 
-    return cnn_model, test_ds
+    return cnn_model, resnet_model, test_ds
 
 
 def main():
     print("Hello from banana!")
-    cnn_model, test_ds = setup()
+    cnn_model, resnet_model, test_ds = setup()
     print("Banana setup complete!")
 
 
